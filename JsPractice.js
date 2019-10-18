88fa47341571e999c515bf9acd83db567720a13f
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2019-10-18 19:06:58 -0400

    js practice underway

--- JsPractice.js
+++ JsPractice.js
@@ -2,30 +2,30 @@
 
 // Write a function that takes an integer minutes and converts it to seconds.
 
-const MinToSeconds = (input) => {
-    const seconds = input * 60;
-    console.log(seconds);
-}
+// const MinToSeconds = (input) => {
+//     const seconds = input * 60;
+//     console.log(seconds);
+// }
 
 // MinToSeconds(5)
 
 // Create a function that takes an array and returns the first element.
 
-const array = [1,2,3,4,5,6]
+// const array = [1,2,3,4,5,6]
 
-const FirstElem = ()  =>   {
-    const first = array[0]
-    console.log(first)
-}
+// const FirstElem = ()  =>   {
+//     const first = array[0]
+//     console.log(first)
+// }
 
 // FirstElem()
 
 // Write a function that takes the base and height of a triangle and return its area.
 
-const triangleArea = (base) =>  {
-    const area = base * 3
-    console.log(area)
-}
+// const triangleArea = (base) =>  {
+//     const area = base * 3
+//     console.log(area)
+// }
 
 // triangleArea(50)
 
@@ -33,14 +33,35 @@ const triangleArea = (base) =>  {
 // the number you passed to the function. For example, if the input is 4 then your function 
 // should return 10 because 1 + 2 + 3 + 4 = 10.EASY
 
-const sumOfNumbers = (input) => {
-    let iterator
-    while(iterator > input) {
-        let iterator = 1
-        iterator += iterator
-        console.log(input)
-        console.log(iterator)
-    }
+// const sumOfNumbers = (input) => {
+//     let iterator
+//     while(iterator > input) {
+//         let iterator = 1
+//         iterator += iterator
+//         console.log(input)
+//         console.log(iterator)
+//     }
+// }
+
+// sumOfNumbers(4)
+
+// Write a function that takes the base and height of a triangle and return its area.
+
+// Examples
+// triArea(3, 2) ➞ 3
+
+// triArea(7, 4) ➞ 14
+
+// triArea(10, 10) ➞ 50
+// Notes
+// The area of a triangle is: (base * height) / 2
+// Don't forget to return the result.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+
+const triangleArea = (base, height) =>  {
+    const totalArea = (base * height) / 2
+    console.log(totalArea)
 }
 
-sumOfNumbers(4)
+triangleArea(6, 8)
